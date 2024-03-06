b66ad62070f8c0145e71e246aa83c122617d079b
AndyLeq129 <97884873+AndyLeq129@users.noreply.github.com>
2024-03-06 10:26:38 -0500

    Start on card shooting

---
+++ project_ink/Assets/Scripts/Andy/PlayerShootingController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShootingController : MonoBehaviour
+{
+    public GameObject[] cards;
+    public float offset;
+    public Transform shotPoint;
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+
+    private int index;
+
+    private void Update()
+    {
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
+
+        if (timeBtwShots <= 0)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Instantiate(cards[index], shotPoint.position, transform.rotation);
+                index++;
+                if (index >= cards.Length)
+                {
+                    //shuffle if card used all
+                    index = 0;
+                }
+
+                timeBtwShots = startTimeBtwShots;
+            }
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+    }
+
+    public void Shoot()
+    {
+        
+    }
+}
