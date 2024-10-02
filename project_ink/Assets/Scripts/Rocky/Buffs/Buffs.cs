91f26d4015738f226bc6bb1eb1f5c1adc71bb5c8
Sapiens-wx <wx2194@nyu.edu>
2024-10-02 16:40:35 -0400

    给buff 5效果射弹添加0.3s延迟

--- project_ink/Assets/Scripts/Rocky/Buffs/Buffs.cs
+++ project_ink/Assets/Scripts/Rocky/Buffs/Buffs.cs
@@ -100,11 +100,17 @@ public class Buff1_5 : Buff{
         if(!enabled) Enable();
         buffs.Add(val);
     }
+    IEnumerator DelayShoot(){
+		yield return new WaitForSeconds(0.3f);
+		CardSlotManager.instance.InstantiateProjectile(2).gameObject.name="buff 5";
+    }
     public void Activate(){
         if(!enabled) return;
         for(int i=0;i<buffs.Count;){
             Activated();
-            CardSlotManager.instance.InstantiateProjectile(2).gameObject.name="buff 5";
+			//uncomment this line after adding enemies
+            //CardSlotManager.instance.InstantiateProjectile(2).gameObject.name="buff 5";
+            CardSlotManager.instance.StartCoroutine(DelayShoot());
             --buffs[i];
             if(buffs[i]==0){
                 buffs[i]=buffs[^1];
