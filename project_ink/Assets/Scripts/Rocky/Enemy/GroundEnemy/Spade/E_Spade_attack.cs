704e5c0a5bc5d975bbea92b0774d61aad7c3fef4
Sapiens-wx <wx2194@nyu.edu>
2024-12-08 11:02:37 -0500

    Spade敌人基本完成

---
+++ project_ink/Assets/Scripts/Rocky/Enemy/GroundEnemy/Spade/E_Spade_attack.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class E_Spade_attack : StateBase<E_Spade>
+{
+    Coroutine dashCoro;
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateEnter(animator, stateInfo, layerIndex);
+        dashCoro=ctrller.StartCoroutine(Dash());
+    }
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateExit(animator, stateInfo, layerIndex);
+        dashCoro=StopCoroutineIfNull(dashCoro);
+    }
+    IEnumerator Dash(){
+        float dashSpd=ctrller.dashDist/ctrller.dashInterval;
+        ctrller.rgb.velocity=new Vector2(ctrller.Dir==1?dashSpd:-dashSpd, 0);
+        yield return new WaitForSeconds(ctrller.dashInterval);
+        ctrller.rgb.velocity=Vector2.zero;
+        ctrller.animator.SetTrigger("attack_end");
+    }
+}
