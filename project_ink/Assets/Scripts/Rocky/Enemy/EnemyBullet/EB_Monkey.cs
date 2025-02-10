c7fc6bffc3a353f51508cda51ceaebded62e41e2
Sapiens-wx <wx2194@nyu.edu>
2025-02-10 17:51:49 -0500

    monkey's projectile's split angle becomes a variable

--- project_ink/Assets/Scripts/Rocky/Enemy/EnemyBullet/EB_Monkey.cs
+++ project_ink/Assets/Scripts/Rocky/Enemy/EnemyBullet/EB_Monkey.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using UnityEngine;
 
 public class EB_Monkey : EnemyBulletBase {
-    [SerializeField] float splitDist;
+    [SerializeField] float splitDist, splitAngle;
     internal override void Start(){
         base.Start();
         StartCoroutine(SplitAfterDist());
@@ -10,7 +10,7 @@ public class EB_Monkey : EnemyBulletBase {
     IEnumerator SplitAfterDist(){
         yield return new WaitForSeconds(splitDist/spd);
         Vector2 dir=rgb.velocity.normalized;
-        float cos=1.732051f, sin=.5f; //cos(PI/6) and sin(PI/6). 30 degree
+        float cos=Mathf.Cos(splitAngle*Mathf.Deg2Rad), sin=Mathf.Sin(splitAngle*Mathf.Deg2Rad); //cos(PI/6) and sin(PI/6). 30 degree
         EnemyBulletManager.InstantiateBullet_dir(EnemyBulletManager.inst.monkey_split, transform.position, new Vector2(dir.x*cos-dir.y*sin, dir.x*sin+dir.y*cos));
         EnemyBulletManager.InstantiateBullet_dir(EnemyBulletManager.inst.monkey_split, transform.position, new Vector2(dir.x*cos+dir.y*sin, -dir.x*sin+dir.y*cos));
         Destroy(gameObject);
