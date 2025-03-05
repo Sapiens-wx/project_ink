81fa6f537500183b090209f6cd3a4731351d55d9
Sapiens-wx <wx2194@nyu.edu>
2025-03-05 11:18:04 -0500

    add player health bar

--- project_ink/Assets/Scripts/Rocky/Enemy/EnemyBullet/EnemyBulletBase.cs
+++ project_ink/Assets/Scripts/Rocky/Enemy/EnemyBullet/EnemyBulletBase.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EnemyBulletBase : MonoBehaviour
 {
     public float spd;
+    public int damage=1;
     [HideInInspector] public Rigidbody2D rgb;
     public event System.Action<EnemyBulletBase,Collider2D> onTriggerEnter;
     internal virtual void Start(){
