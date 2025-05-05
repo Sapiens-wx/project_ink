e896acc34cdcf13a1847e7fd286098e35bc16831
Sapiens-wx <wx2194@nyu.edu>
2025-05-05 17:40:15 -0400

    fix bug

--- project_ink/Assets/Scripts/Rocky/Enemy/Boss/Boss1/B1_1_A4_Bullet.cs
+++ project_ink/Assets/Scripts/Rocky/Enemy/Boss/Boss1/B1_1_A4_Bullet.cs
@@ -11,12 +11,12 @@ public class B1_1_A4_Bullet : EnemyBulletBase
         StartCoroutine(DelayDestroy());
         Vector2 flyingDir=rgb.velocity.normalized;
         Ray ray=new Ray(transform.position, flyingDir);
-        if(collider.bounds.IntersectRay(ray, out float dist)){
-            Vector3 bulletPos=(Vector2)transform.position-flyingDir*.3f;
+        collider.bounds.IntersectRay(ray, out float dist);//{
             flyingDir=-MathUtil.CalculateIntersectionNormal(ray, collider.bounds);
+            Vector3 bulletPos=(Vector2)transform.position+flyingDir*.3f;
             EnemyBulletManager.InstantiateBullet_dir(EnemyBulletManager.inst.boss1_a5_2,bulletPos,flyingDir);
             EnemyBulletManager.InstantiateBullet_dir(EnemyBulletManager.inst.boss1_a5_2,bulletPos,MathUtil.Rotate(flyingDir,bullet_angle*Mathf.Deg2Rad));
             EnemyBulletManager.InstantiateBullet_dir(EnemyBulletManager.inst.boss1_a5_2,bulletPos,MathUtil.Rotate(flyingDir,-bullet_angle*Mathf.Deg2Rad));
-        }
+        //}
     }
 }
