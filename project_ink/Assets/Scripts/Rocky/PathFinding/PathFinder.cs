feaded7589b24245d6cfd0a7e9cdad2937400fbf
Sapiens-wx <wx2194@nyu.edu>
2025-07-03 20:06:19 +0800

    * enemy now jumps normally * player can now move with the moving platform

--- project_ink/Assets/Scripts/Rocky/PathFinding/PathFinder.cs
+++ project_ink/Assets/Scripts/Rocky/PathFinding/PathFinder.cs
@@ -593,12 +593,12 @@ public class PathNavigator {
             return Vector2.zero;
         }
         //calculate the exact [to] position: just when the ctrller's corner touches the platform's corner
-        Vector2 min=hit.collider.bounds.min, max=hit.collider.bounds.max;
         Vector2 bcExtents=ctrller.bc.bounds.extents, bcOffset=ctrller.bc.offset;
         if(ctrller.Dir==1){ //jump right up
-            to=new Vector2(min.x-bcExtents.x+bcOffset.x, max.y+bcExtents.y+bcOffset.y);
+            to.x-=PathFinder.inst.GridSize.x;
         } else //jump left up
-            to=new Vector2(max.x+bcExtents.x+bcOffset.x, max.y+bcExtents.y+bcOffset.y);
+            to.x+=PathFinder.inst.GridSize.x;
+        to.y=hit.point.y+bcExtents.y;
         //calculate velocity
         Vector2 v=Vector2.zero;
         float jumpTime=.5f;
@@ -606,7 +606,7 @@ public class PathNavigator {
         float gravity=ctrller.rgb.gravityScale*9.8f;
         v.y=h/jumpTime+.5f*gravity*jumpTime; //v=h/t+.5*gt
         v.x=(to.x-from.x)/jumpTime+.1f;
-        v.y+=.3f/Mathf.Max(v.y,1); //even with exact value, the enemy still jumps lower than desired height
+        v.y+=1; //even with exact value, the enemy still jumps lower than desired height
         return v;
     }
     PathFinder.Node NearestNodeToTarget(Transform target){
--- project_ink/Assets/Scripts/Rocky/Traps/Trap_MovingPlatform.cs
+++ project_ink/Assets/Scripts/Rocky/Traps/Trap_MovingPlatform.cs
@@ -5,6 +5,10 @@ public class Trap_MovingPlatform : TrapBase
 {
     [SerializeField] float spd;
     [SerializeField] Vector2 start,end;
+
+    Collider2D bc;
+    Vector2 prevPos;
+    bool prevHit;
     void OnDrawGizmosSelected(){
         Gizmos.color=Color.green;
         Gizmos.DrawLine(start,end);
@@ -15,5 +19,26 @@ public class Trap_MovingPlatform : TrapBase
         Sequence s=DOTween.Sequence();
         float dist=Vector2.Distance(start,end);
         transform.DOMove(end,dist/spd).SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo);
+    }
+    void FixedUpdate(){
+        //ray origin: top left of the bounds. distance: bounds.size.x
+        Vector2 topLeft=new Vector2(bc.bounds.min.x,bc.bounds.max.y+MathUtil.colliderMinGap);
+        RaycastHit2D hit=Physics2D.Raycast(topLeft,Vector2.right,bc.bounds.size.x,GameManager.inst.playerLayer);
+        Vector2 velocity=((Vector2)transform.position-prevPos)/Time.fixedDeltaTime;
+        //if player enters on the platform, activate the platform
+        if(!hit && prevHit){ //player exits platform
+            PlayerCtrl.inst.v_trap_platform=Vector2.zero;
+        }
+        //what player stays on the platform, update player.v_trap_platform
+        if(hit){
+            PlayerCtrl.inst.v_trap_platform=velocity;
+        }
+        prevHit=hit;
+        prevPos=transform.position;
+    }
+    public override void ChangeTheme(Theme theme)
+    {
+        base.ChangeTheme(theme);
+        bc=activeSprite.GetComponent<Collider2D>();
     }
 }
